48f624da4881d7bedce6353a195a3092e8553dec
elenoack <elenanoack@mail.ru>
2022-05-30 11:24:33 +0300

    Initial Commit

---
+++ SettingsScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SettingsScreen
+//
+//  Created by mac on 30.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
